0b2f88c355e5f98feed1d859dcf72cbb4be5b8c3
urevanescence <screav@gmail.com>
2026-05-11 23:12:20 +0600

    Reviewing material after a one-week break

--- sql/day_08_inner_join.sql
+++ sql/day_08_inner_join.sql
@@ -33,3 +33,65 @@ FROM orders AS o
 INNER JOIN returns AS r 
   ON o.order_id = r.order_id
 WHERE o.profit < 0;
+
+-- PRACTICE AND REVIEWING MATERIAL AFTER A WEEK BREAK
+
+-- Task 1: Furniture tax report (10% tax)
+SELECT
+    p.product_id,
+    o.order_id,
+    p.product_name,
+    p.category, 
+    ROUND (o.sales, 2) AS sales,
+    ROUND((o.sales * 0.1), 2) AS tax
+FROM orders AS o
+INNER JOIN products AS p
+  ON o.product_id = p.product_id
+WHERE p.category = 'Furniture'
+ORDER BY o.sales DESC;
+
+-- Task 2: Triple Join - Total sales by customer and category
+SELECT
+    c.customer_name,
+    p.category,
+    ROUND(SUM(o.sales), 2) AS total_sales
+FROM orders AS o
+INNER JOIN products AS p
+  ON o.product_id = p.product_id
+INNER JOIN customers AS c
+  ON o.customer_id = c.customer_id
+GROUP BY c.customer_name, p.category
+ORDER BY total_sales DESC;
+
+-- Task 3: High discounts (>20%) in Office Supplies
+SELECT
+    o.order_id,
+    p.product_id,
+    p.product_name,
+    ROUND(o.discount, 2) AS discount,
+    ROUND(o.sales, 2) AS sales
+FROM orders AS o
+INNER JOIN products AS p
+  ON o.product_id = p.product_id
+WHERE p.category = 'Office Supplies' AND o.discount > 0.2
+ORDER BY discount DESC;
+
+-- Task 4: Corporate Phones profitability report
+SELECT
+    c.customer_id,
+    c.customer_name,
+    p.product_id,
+    p.product_name,
+    ROUND(SUM(o.profit), 2) AS total_profit
+FROM orders AS o
+INNER JOIN products AS p
+  ON o.product_id = p.product_id
+INNER JOIN customers AS c
+  ON o.customer_id = c.customer_id
+WHERE c.segment = 'Corporate' AND p.product_name ILIKE '%Phone%'
+GROUP BY
+    c.customer_id,
+    c.customer_name,
+    p.product_id,
+    p.product_name
+ORDER BY total_profit DESC;
